4b41a0ac878dc879a5c9bdcc3d3296f0a8262a9c
piyushmishraa <fanboiofharrystyles@gmail.com>
2024-05-08 19:20:41 -0700

    added cloudinary for url of photos and multer for discstorage for temporary purpose

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,27 @@
+import {v2 as cloudinary} from 'cloudinary';
+import fs from "fs"
+
+cloudinary.config({ 
+    cloud_name:dwl2hjsga , 
+    api_key: process.env.CLOUDINARY_API_KEY, 
+    api_secret: CLOUDINARY_API_SECRET // Click 'View Credentials' below to copy your API secret
+});
+
+
+const uploadOnCloudinary= async(localFilePath)=>{
+    try {
+        if(!localFilePath) return null
+        const response =await cloudinary.uploader.upload(localFilePath,{
+            resource_type:"auto"
+        })
+        console.log(response.url);
+        return response;
+        
+    } catch (error) {
+        fs.unlinkSync(localFilePath)
+    }
+}
+    
+
+
+export {uploadOnCloudinary};
