d80f393e780846d3c400cf71d1a63afba2cb9cfc
piyushmishraa <fanboiofharrystyles@gmail.com>
2024-05-13 19:23:23 -0700

    created user controller for saving data

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,21 @@
 import asyncHandler from "../utils/asyncHandler.js"
 
 const registerUser=asyncHandler(async(req,res)=>{
-    res.status(200).json({
-        message:"fucking fine"
-    })
+      //get user details
+      //validate fields
+      //check if userexist from username or email
+      //check for avatar
+      //check for userprofile pic
+      //upload them to cloudinary but first to local disc storage via multer and delete it from there
+      //create user object and create entry in db
+      //remove password and refresh token from response
+      //check if response is created or not 
+      //send response to user 
+     const{username,fullname,email}= req.body;
+     console.log(username,email);
 })
 
 export {registerUser}
+
+
+
