a71034fc0e7a72bbb356ea8f3b1967a332d13ff4
piyushmishraa <fanboiofharrystyles@gmail.com>
2024-06-22 08:16:51 -0700

    added subscription model

---
+++ src/models/subscription.models.js
@@ -0,0 +1,16 @@
+import mongoose, { model } from "mongoose";
+
+const subcriptionSchema= new mongoose.Schema({
+
+    subscriber:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:User
+    },
+    channel:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:User
+    }
+
+},{timestamps:true});
+
+export const Subscription = mongoose.model("Subsciption",subcriptionSchema);
